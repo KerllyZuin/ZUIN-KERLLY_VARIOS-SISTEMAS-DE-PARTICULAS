8d512120efc2d9376fc95c9e630e101d8fb3e976
KerllyZuin <kerlly152@gmail.com>
2023-12-21 23:04:25 -0500

    Se creo el archivo Particula.js Sistema.js sketch.js

---
+++ classes/Sistema.js
@@ -0,0 +1,60 @@
+class Sistema {
+  constructor() {
+    this.pos = createVector(width / 2, height / 2);
+    this.ps = [];
+    this.t = random(100);
+    this.colors = [
+      color(random(100, 255), random(255), 0),
+      color(random(255), 0, random(100, 255)),
+    ];
+  }
+
+  update() {
+    this.t += 0.01;
+    this.pos.x = map(noise(this.t), 0, 1, 0, width);
+    this.pos.y = map(noise(this.t + 10), 0, 1, 0, height);
+
+    let colorIndex = Math.floor(random(this.colors.length));
+    let color = this.colors[colorIndex];
+
+    // Crear partícula de círculo
+    let circleParticle = new Particula(
+      this.pos.x,
+      this.pos.y,
+      color,
+      "circle"
+    );
+    this.ps.push(circleParticle);
+
+    // Crear partícula de rectángulo
+    let rectangleParticle = new Particula(
+      this.pos.x,
+      this.pos.y,
+      color,
+      "rectangle"
+    );
+    this.ps.push(rectangleParticle);
+
+    // Crear partícula de triángulo
+    let triangleParticle = new Particula(
+      this.pos.x,
+      this.pos.y,
+      color,
+      "triangle"
+    );
+    this.ps.push(triangleParticle);
+
+    for (let i = this.ps.length - 1; i >= 0; i--) {
+      if (!this.ps[i].isAlive) {
+        this.ps.splice(i, 1);
+      }
+    }
+  }
+
+  display() {
+    for (let i = 0; i < this.ps.length; i++) {
+      this.ps[i].update();
+      this.ps[i].display();
+    }
+  }
+}
